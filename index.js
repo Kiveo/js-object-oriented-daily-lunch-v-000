f71bc5dff9267a685658b22b108ea17660502aca
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 20:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ class Neighborhood {
     this.id = ++neighborhoodId;
     store.neighborhoods.push(this);
   }
+  deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.neighborhoodId === this.id;
+    })
+  }
 }
 
 class Customer {
