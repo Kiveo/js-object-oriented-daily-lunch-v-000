eccfb7997862eac9cf702a256b6304448b973f57
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 20:54:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ class Customer {
   }
 
   totalSpent(){
-    return this.meals().reduce(function(accumulator, currentValue) {
-      return accumulator + currentValue;
+    return this.meals().reduce(function(accumulator, 0) {
+      return accumulator + meal.price;
     });
   }
 }
